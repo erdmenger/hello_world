2efe71fc040207191220bf6b1d94ccd93804a06d
erdmenger <erdmenger@users.noreply.github.com>
2015-11-22 22:22:23 +0100

    o $Id:

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@
 'Joerg Erdmenger, 2015-11-21'
 /**
   hello.js little Javascript program to learn git.
-  ' $Id: ,v 1.0 1998/10/05 14:55:03 erde Exp erde $ "Erdmenger"
+  $Id: ,v 1.0 1998/10/05 14:55:03 erde Exp erde $ "Erdmenger"
 */
 
 console.log( 'hello' );
