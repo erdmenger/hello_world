1afcccae328eacb6fc93e2c7ff4bd0b0d24ff933
erdmenger <erdmenger@users.noreply.github.com>
2015-11-22 22:21:21 +0100

    o initial version

---
+++ hello.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+'Joerg Erdmenger, 2015-11-21'
+/**
+  hello.js little Javascript program to learn git.
+  ' $Id: ,v 1.0 1998/10/05 14:55:03 erde Exp erde $ "Erdmenger"
+*/
+
+console.log( 'hello' );
