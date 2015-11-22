210c270feb8d5dfd104a1ac0a8a2f94562892951
erdmenger <erdmenger@users.noreply.github.com>
2015-11-22 22:24:02 +0100

    o try 1 with vc Id

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-'Joerg Erdmenger, 2015-11-21'
+'$Id: ,v 1.0 1998/10/05 14:55:03 erde Exp erde $ "Erdmenger"'
 /**
   hello.js little Javascript program to learn git.
   $Id: ,v 1.0 1998/10/05 14:55:03 erde Exp erde $ "Erdmenger"
